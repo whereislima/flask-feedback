98784318e60e2714a8d9ad6cc93265a8aeee55dc
whereislima <reach.lima@gmail.com>
2021-05-23 16:39:37 -0700

    feedback model progress

---
+++ app.py
@@ -0,0 +1,104 @@
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User, Feedback
+from forms import RegisterForm, LoginForm, FeedbackForm
+from werkzeug.exceptions import Unauthorized
+from sqlalchemy.exc import IntegrityError
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///flask_feedback"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "mangopudding"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+connect_db(app)
+
+toolbar = DebugToolbarExtension(app)
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register_user():
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username=form.username.data
+        password=form.password.data
+        email=form.email.data
+        first_name=form.first_name.data
+        last_name=form.last_name.data
+
+        user = User.register(username, password, email, first_name, last_name)
+        
+        db.session.add(user)
+        db.session.commit()
+        # not registering new users with or without below
+        # try:
+        #     db.session.commit()
+        # except IntegrityError:
+            
+        #     form.username.errors.append('username already taken.')
+        #     return render_template('register.html', form=form)
+
+        session['username'] = user.username
+        flash('Successfully created your account')
+        return redirect(f"/users/{user.username}")
+    else:
+        return render_template("register.html", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login_user():
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        username=form.username.data
+        password=form.password.data
+
+        user = User.authenticate(username, password)
+        if user:
+            session['username'] = user.username
+            return redirect(f"/users/{user.username}")
+        else:
+            form.username.errors = ['Invalid username/password']
+    else:
+        return render_template("login.html", form=form)
+
+@app.route("/users/<username>")
+def secret(username):
+    
+    if "username" not in session or username != session['username']:
+        raise Unauthorized()
+
+    user = User.query.get_or_404(username)
+    all_feedback = Feedback.query.all()
+
+    return render_template("users/detail.html", user=user, all_feedback=all_feedback)
+    
+
+@app.route("/logout")
+def logout():
+    session.pop('username')
+    return redirect("/")
+
+@app.route("/users/<username>/delete")
+def delete_user(username):
+
+    if "username" not in session or username != session['username']:
+        raise Unauthorized()
+
+    user = User.query.get_or_404(username)
+
+    db.session.commit.delete(user)
+    db.session.commit()
+    session.pop("username")
+
+    return redirect("/login")
+
+@app.route("/users/<username>/feedback/add", methods=["GET", "POST"])
+def add_feedback(username):
+
+    form = FeedbackForm()
+    return render_template("users/add_feedback.html", form=form)
---
+++ models.py
@@ -0,0 +1,63 @@
+"""Models for flash-feedback app."""
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+def connect_db(app):
+    """Connect this database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User model"""
+
+    __tablename__ = "users"
+
+    username = db.Column(db.String(20), primary_key=True, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+    feedback = db.relationship("Feedback", backref="user", cascade="all,delete")
+
+
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name):
+        """Register user w/hashed password & return user."""
+
+        hashed = bcrypt.generate_password_hash(password)
+        # turn bytestring into normal (unicode utf8) string
+        hashed_utf8 = hashed.decode("utf8")
+
+        # return instance of user w/username and hashed pwd
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validate that user exists & password is correct.
+
+        Return user if valid; else return False.
+        """
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            # return user instance
+            return user
+        else:
+            return False
+
+class Feedback(db.Model):
+
+    __tablename__ = "feedback"
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(db.String(20), db.ForeignKey('users.username'), nullable=False)
+
